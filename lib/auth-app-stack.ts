4d88e177d929fcd0a71cbb568be23081acf0efb2
Denis <marincasdenis@yahoo.com>
2024-10-31 16:58:20 +0000

    Sign-in and signout resources.

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -37,6 +37,16 @@ export class AuthAppStack extends cdk.Stack {
     });
 
     this.auth = authApi.root.addResource("auth");
+
+    this.addAuthRoute("signup", "POST", "SignupFn", "signup.ts");
+    this.addAuthRoute(
+      "confirm_signup",
+      "POST",
+      "ConfirmFn",
+      "confirm-signup.ts"
+    );
+    this.addAuthRoute("signout", "GET", "SignoutFn", "signout.ts");
+    this.addAuthRoute("signin", "POST", "SigninFn", "signin.ts");
   }
 
   private addAuthRoute(
